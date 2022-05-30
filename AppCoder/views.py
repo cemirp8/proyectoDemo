8e6ea856f9c38ebe1ef762e490bfd20920cfc988
cemirp8 <camilar862@gmail.com>
2022-05-30 10:46:16 -0500

    Primera version

---
+++ AppCoder/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from AppCoder.models import Curso
+
+def curso(self):
+    curso=Curso(nombre="Desarrollo Web", camada=16740)
+    curso.save()
+    documento = f"Curso: {curso.nombre} - Camada: {curso.camada}"
+    return HttpResponse(documento)
